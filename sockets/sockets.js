9aab8c2cd0250539c21cb8b1833c8f05ac8b31a4
JhonSanz <ingjhonsanz@utp.edu.co>
2020-02-12 19:55:20 -0500

    socket emit

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,5 +1,10 @@
 const io = require('../index')
+const { Post } = require('../models/posts/post')
 
 io.on('connection', function (socket) {
-    console.log('a user connected');
+    Post.find({}, (err, posts) => {
+        socket.emit('enviarMensaje', {
+            posts: posts
+        });
+    })
 });

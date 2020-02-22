58737b33384bb9a9689da8ea45eddc4ec9f81aaa
JhonSanz <ingjhonsanz@utp.edu.co>
2020-02-22 10:52:38 -0500

    socket names

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -3,14 +3,14 @@ const { Post } = require('../models/posts/post')
 
 io.on('connection', function (socket) {
     Post.find({}, (err, posts) => {
-        socket.emit('enviarMensaje', {
+        socket.emit('get_posts', {
             posts: posts
         });
     })
     socket.on('created', function (data, fn) {
         Post.findById(data.identifier, function (err, post) {
-            socket.emit('NEW', post);
-            socket.broadcast.emit('NEW', post);
+            socket.emit('created_post', post);
+            socket.broadcast.emit('created_post', post);
         });
     });
 });
